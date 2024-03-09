ed5b9691ea495033ef342efff89789de939575d9
Ilan Joselevich <personal@ilanjoselevich.com>
2024-03-09 20:57:31 +0200

    Store data on filesystem rather than storage
    This helps avoiding missing notifications about updates.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use regex::Regex;
-use std::{collections::HashSet, error::Error};
+use serde::{Deserialize, Serialize};
+use std::{collections::HashSet, error::Error, fs::File, io::BufReader};
 
 const PAGE_URL: &str = "http://taharut.org/imo/";
 
-#[derive(Hash, Eq, PartialEq, Clone, Debug)]
+#[derive(Hash, Eq, PartialEq, Clone, Serialize, Deserialize)]
 pub struct Exercise {
     pub name: String,
     pub author: String,
@@ -11,22 +12,23 @@ pub struct Exercise {
 }
 
 impl Exercise {
-    pub fn build(name: String, author: String, url: String) -> Self {
+    pub fn new(name: String, author: String, url: String) -> Self {
         Exercise { name, author, url }
     }
 }
 
-#[derive(Clone)]
-pub struct Exercises(HashSet<Exercise>);
+pub struct Exercises {
+    file: File,
+}
 
 impl Exercises {
-    #[allow(clippy::new_without_default)]
-    pub fn new() -> Self {
-        Exercises(HashSet::new())
+    pub fn new(file: File) -> Self {
+        Self { file }
     }
 
-    pub fn get(&self) -> &HashSet<Exercise> {
-        &self.0
+    pub fn get(&self) -> HashSet<Exercise> {
+        let reader = BufReader::new(&self.file);
+        serde_json::from_reader(reader).unwrap_or_default()
     }
 
     pub fn update(&mut self) -> Result<(), Box<dyn Error>> {
@@ -34,17 +36,19 @@ impl Exercises {
 
         let re = Regex::new(r#"<a href = "([^"]+)">([^<]+)</a>([^<]+)<br>"#)?;
 
-        self.0 = re
-            .captures_iter(&res)
-            .map(|c| {
-                let c = c.extract::<3>().1;
-                Exercise::build(
-                    c[1].trim().to_string(),
-                    c[2].trim().to_string(),
-                    PAGE_URL.to_string() + c[0],
-                )
-            })
-            .collect();
+        serde_json::to_writer_pretty(
+            &self.file,
+            &re.captures_iter(&res)
+                .map(|c| {
+                    let c = c.extract::<3>().1;
+                    Exercise::new(
+                        c[1].trim().to_string(),
+                        c[2].trim().to_string(),
+                        PAGE_URL.to_string() + c[0],
+                    )
+                })
+                .collect::<HashSet<Exercise>>(),
+        )?;
 
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,41 @@
-use std::{error::Error, mem, thread::sleep, time::Duration};
+use std::{fs::File, thread::sleep, time::Duration};
 mod cli;
 use clap::Parser;
 use cli::Args;
-use log::{info, warn};
+use directories::ProjectDirs;
+use log::{error, info, warn};
 use notify_rust::Notification;
 use taharut::Exercises;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let args = Args::parse();
 
     env_logger::builder()
         .filter_level(log::LevelFilter::Info)
         .init();
 
-    let mut exercises = Exercises::new();
+    let project_dirs = ProjectDirs::from("com", clap::crate_authors!(), clap::crate_name!())
+        .unwrap_or_else(|| {
+            error!("Failed to determine project directory");
+            std::process::exit(1);
+        });
+
+    if let Err(e) = std::fs::create_dir_all(project_dirs.cache_dir()) {
+        error!("Failed to create cache directory: {}", e);
+        std::process::exit(1);
+    }
+
+    let mut exercises = Exercises::new(
+        File::options()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(project_dirs.cache_dir().join("data.json"))
+            .unwrap_or_else(|e| {
+                error!("Failed to open data file: {}", e);
+                std::process::exit(1);
+            }),
+    );
 
     loop {
         let old_exercise = exercises.get().clone();
@@ -21,7 +43,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         match exercises.update() {
             Ok(_) if old_exercise.is_empty() => info!("Successfully updated initial exercises"),
             Err(e) if old_exercise.is_empty() => {
-                warn!("Failed to update initial exercises with error: {}", e)
+                warn!("Failed to update initial exercises with: {}", e)
             }
             Ok(_) => {
                 for d in exercises.get().difference(&old_exercise) {
@@ -29,16 +51,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 
                     info!("{}", message);
 
-                    Notification::new()
+                    if let Err(e) = Notification::new()
                         .summary("Taharut: New exercise available!")
                         .body(&message)
-                        .show()?;
+                        .show()
+                    {
+                        warn!("Failed to show notification: {}", e);
+                    }
                 }
             }
-            Err(e) => warn!("Failed to fetch exercises with error: {}", e),
+            Err(e) => warn!("Failed to fetch exercises: {}", e),
         }
 
-        mem::drop(old_exercise);
+        drop(old_exercise);
 
         sleep(Duration::from_secs(args.interval as u64 * 60));
     }
